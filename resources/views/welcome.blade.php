e2f0750a1f2b5a750a84217485a1ebb1b36e5c8e
ananas32 <porsche839@gmail.com>
2017-10-09 12:25:26 +0300

    add list from table

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,92 +4,80 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <title>vue table load</title>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
+        {{--<div class="flex-center position-ref full-height">--}}
+            {{--@if (Route::has('login'))--}}
+                {{--<div class="top-right links">--}}
+                    {{--@if (Auth::check())--}}
+                        {{--<a href="{{ url('/home') }}">Home</a>--}}
+                    {{--@else--}}
+                        {{--<a href="{{ url('/login') }}">Login</a>--}}
+                        {{--<a href="{{ url('/register') }}">Register</a>--}}
+                    {{--@endif--}}
+                {{--</div>--}}
+            {{--@endif--}}
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+        {{--</div>--}}
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+        <div id="app">
+            <users></users>
         </div>
+
+        <template id="users-template">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>name</th>
+                    <th>email</th>
+                    <th>create at</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="user in users">
+                        <td>@{{ user.id }}</td>
+                        <td>@{{ user.name }}</td>
+                        <td>@{{ user.email }}</td>
+                        <td>@{{ user.created_at }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </template>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="https://unpkg.com/vue/dist/vue.js"></script>
+        <script>
+            Vue.component('users', {
+                template: '#users-template',
+
+                data: function() {
+                    return {
+                        users: []
+                    }
+                },
+
+                created: function() {
+                    this.getUsers();
+                },
+
+                methods: {
+                    getUsers:function () {
+                        $.getJSON("{{ route('api_users') }}", function(users){
+                            this.users = users;
+                        }.bind(this));
+                        setTimeout(this.getUsers, 5000);
+                    }
+                }
+            });
+            new Vue({
+                el: "#app"
+            });
+        </script>
     </body>
 </html>
